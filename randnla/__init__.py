f50cdb8cf692ec29e22d55f8c98726cf4b8f857e
Tyler <chentylerl@gmail.com>
2025-09-11 08:27:51 -0400

    add RBKI example

--- randnla/__init__.py
+++ randnla/__init__.py
@@ -1,9 +1,11 @@
 import numpy as np
 import scipy as sp
 
+from .lanczos import *
 from .sketching import *
 from .iterative import *
 from .regression import *
 from .factorization import *
+from .lowrank import *
 from .trace import *
 from .style import *
